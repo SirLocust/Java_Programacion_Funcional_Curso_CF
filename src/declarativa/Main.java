3cbb46574be5c046bd040bf86618d620aa1a04e0
sirlocust <jua56yu@hotmail.com>
2020-08-17 17:37:42 -0500

    Filtrar elementos

--- src/declarativa/Main.java
+++ src/declarativa/Main.java
@@ -8,26 +8,21 @@ public class Main {
     public static void main(String []args) {
 
         List<User> users = new ArrayList<>();
-        //Obtener la cantidad de usuarios con una edad mayor a 18.
-        users.add(new User("rafael", 25));
+        // Filtrar
+        //Obtener la cantidad de usuarios con una edad mayor a 18 y que su username comienze con mayuscula.
+        users.add(new User("Rafael", 25));
         users.add(new User("daniel", 20));
         users.add(new User("Tania", 15));
         users.add(new User("Luisa", 24));
         users.add(new User("Raul", 12));
 
+        long count = users.stream()
+                        .filter( user -> user.getAge() > 18 )
+                        .filter( user -> user.getUsername().startsWith("R") )
+                        .count();
+        System.out.println(count);
 
-        //Iemperativo
-        int count = 0;
-        for (int i = 0; i < users.size() ; i++) {
-            if(users.get(i).getAge() > 18){
-                count++;
-            }
-        }
-        System.out.println("la cantidad de usuarios mayores de edad son "+ count);
 
-        //Declarativo
-        long contador = users.stream().filter( user  -> user.getAge() > 18).count();
-        System.out.println("la cantidad de usuarios mayores de edad son "+ contador);
 
     }
 }
